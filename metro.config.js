2fa7dc5045a801653d1c3e000278a2441c88d104
samchencode <samchencode@gmail.com>
2022-11-19 06:09:46 -0500

    build: add ejs

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,13 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
+// resolve .ejs files
+config.resolver.assetExts.push('ejs');
+
+// resolve nodejs modules as empty to silence errors from ejs package
+config.resolver.extraNodeModules['fs'] = config.resolver.emptyModulePath;
+config.resolver.extraNodeModules['path'] = config.resolver.emptyModulePath;
+
 // preserve argument names for dependnecy injection
 config.transformer.minifierConfig.mangle = false;
 

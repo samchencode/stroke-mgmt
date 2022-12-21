a77b59fa5aa51a29b1774e5f2038f2f2706e6420
samchencode <samchencode@gmail.com>
2022-12-20 22:16:08 -0500

    refactor: export from index

--- src/domain/models/Algorithm/index.ts
+++ src/domain/models/Algorithm/index.ts
@@ -4,3 +4,10 @@ export { ScoredAlgorithm } from '@/domain/models/Algorithm/ScoredAlgorithm';
 export { AlgorithmInfo } from '@/domain/models/Algorithm/AlgorithmInfo';
 export { AlgorithmId } from '@/domain/models/Algorithm/AlgorithmId';
 export { Outcome } from '@/domain/models/Algorithm/Outcome';
+export type { AlgorithmRepository } from '@/domain/models/Algorithm/ports/AlgorithmRepository';
+export {
+  GreaterThanCriterion,
+  LessThanCriterion,
+} from '@/domain/models/Algorithm/Criterion';
+export { Switch } from '@/domain/models/Algorithm/Switch';
+export { SwitchId } from '@/domain/models/Algorithm/SwitchId';

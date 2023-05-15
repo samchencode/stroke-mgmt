d1c5df90fcba0c8d2a4662ad40b0de45a6aa70ad
samchencode <samchencode@gmail.com>
2023-05-14 22:46:39 -0400

    fix: import

--- src/domain/models/Tag/index.ts
+++ src/domain/models/Tag/index.ts
@@ -1,2 +1,2 @@
 export { Tag } from '@/domain/models/Tag/Tag';
-export type { TagRepository } from '@/domain/models/Tag/TagRepository';
+export type { TagRepository } from '@/domain/models/Tag/ports/TagRepository';

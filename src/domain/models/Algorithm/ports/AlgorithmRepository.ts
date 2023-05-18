2ae415144aa21c5b054d7db864f5fbfb9b701491
samchencode <samchencode@gmail.com>
2023-05-18 17:53:24 -0400

    refactor: remove unused methods from interface

--- src/domain/models/Algorithm/ports/AlgorithmRepository.ts
+++ src/domain/models/Algorithm/ports/AlgorithmRepository.ts
@@ -1,14 +1,10 @@
 import type { Algorithm } from '@/domain/models/Algorithm/Algorithm';
 import type { AlgorithmId } from '@/domain/models/Algorithm/AlgorithmId';
-import type { AlgorithmMetadata } from '@/domain/models/Algorithm/AlgorithmMetadata';
 
 interface AlgorithmRepository {
   getAll(): Promise<Algorithm[]>;
   getById(id: AlgorithmId): Promise<Algorithm>;
   getAllShownOnHomeScreen(): Promise<Algorithm[]>;
-  getAllMetadata(): Promise<AlgorithmMetadata[]>;
-  getMetadataById(id: AlgorithmId): Promise<AlgorithmMetadata>;
-  getMetadataForAllShownOnHomeScreen(): Promise<AlgorithmMetadata[]>;
   isAvailable(): Promise<boolean>;
 }
 

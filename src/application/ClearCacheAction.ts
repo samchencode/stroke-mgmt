0dbea2dedb12019b8f179f118e2f66c6928a776a
samchencode <samchencode@gmail.com>
2023-05-18 17:42:32 -0400

    feat: clear image cache as well

--- src/application/ClearCacheAction.ts
+++ src/application/ClearCacheAction.ts
@@ -1,15 +1,18 @@
+import type { ImageCache } from '@/domain/models/Image';
 import type { ArticleCache, TagCache } from '@/domain/services/Cache';
 
 class ClearCacheAction {
   constructor(
     private readonly articleCache: ArticleCache,
-    private readonly tagCache: TagCache
+    private readonly tagCache: TagCache,
+    private readonly imageCache: ImageCache
   ) {}
 
   async execute() {
     return Promise.all([
       this.tagCache.clearCache(),
       this.articleCache.clearCache(),
+      this.imageCache.clearCache(),
     ]);
   }
 }

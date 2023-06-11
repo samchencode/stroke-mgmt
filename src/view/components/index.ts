bf75136ba63cd5300081a83cd412f9c104163b13
samchencode <samchencode@gmail.com>
2023-06-11 08:11:44 -0400

    refactor: consistent exports

--- src/view/components/index.ts
+++ src/view/components/index.ts
@@ -2,3 +2,4 @@ export { IconButton } from '@/view/components/IconButton';
 export { Button } from '@/view/components/Button';
 export { TextButton } from '@/view/components/TextButton';
 export { LoadingSpinnerView } from '@/view/components/LoadingSpinnerView';
+export { Checkbox } from '@/view/components/Checkbox';

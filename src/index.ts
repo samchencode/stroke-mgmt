8e008205df4abbb3c9f04311ca67915ba236a208
samchencode <samchencode@gmail.com>
2023-11-17 08:47:57 -0500

    fix: promise.allSettled undefined

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import type { Type as App } from '@/view/App/App';
 import { container } from '@/di';
 // crypto.getRandomValues polyfill for React Native for uuid package
 import 'react-native-get-random-values';
+import '@/promisePolyfill';
 
 const Root = container.get<App>('App');
 
---
+++ src/promisePolyfill.js
@@ -0,0 +1,16 @@
+Promise.allSettled =
+  Promise.allSettled ||
+  ((promises) =>
+    Promise.all(
+      promises.map((p) =>
+        p
+          .then((value) => ({
+            status: 'fulfilled',
+            value,
+          }))
+          .catch((reason) => ({
+            status: 'rejected',
+            reason,
+          }))
+      )
+    ));
